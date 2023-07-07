cd9bf6c8c6cc42ed4ce77026ad1dd945f039ec6d
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2023-07-07 03:36:03 -0700

    Add file

---
+++ thaixtransformers/__init__.py
@@ -0,0 +1,14 @@
+def Tokenizer(model_name,**kwag):
+    if "wangchanberta-base-att-spm-uncased" in model_name:
+        from .roberta import WangchanbertaTokenizer as tokenzer
+    elif "wangchanberta-base-wiki-newmm" in model_name:
+        from .core import ThaiWordsNewmmTokenizer as tokenzer
+    elif "wangchanberta-base-wiki-syllable" in model_name:
+        from .core import ThaiWordsSyllableTokenizer as tokenzer
+    elif "wangchanberta-base-wiki-sefr" in model_name:
+        from .core import FakeSefrCutTokenizer as tokenzer
+    elif "wangchanberta-base-wiki-spm" in model_name:
+        from .roberta import ThaiRobertaTokenizer as tokenzer
+    else:
+        from transformers import AutoTokenizer as tokenzer
+    return tokenzer.from_pretrained(model_name,**kwag)
